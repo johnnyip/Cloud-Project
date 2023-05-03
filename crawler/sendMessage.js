d776d651953c4839892ad345abea1030733680b4
unclechuen <johnny@johnnyip.com>
2023-05-03 20:16:49 +0800

    minor

--- crawler/sendMessage.js
+++ crawler/sendMessage.js
@@ -1,34 +1,33 @@
 const kafka = require('kafka-node');
 
-const sendMessage = (topicName, message, kafkaHost = process.env.KAFKA_URL !== undefined ? process.env.KAFKA_URL :'localhost:9092') => {
-    return new Promise((resolve, reject) => {
-      const client = new kafka.KafkaClient({ kafkaHost });
-      const producer = new kafka.Producer(client);
-  
-      producer.on('ready', () => {
-        const payloads = [
-          {
-            topic: topicName,
-            messages: message,
-          },
-        ];
-  
-        producer.send(payloads, (err, data) => {
-          if (err) {
-            reject(err);
-          } else {
-            resolve(data);
-          }
-          producer.close();
-          client.close();
-        });
-      });
-  
-      producer.on('error', (err) => {
-        reject(err);
+const sendMessage = (topicName, message, kafkaHost = 'kafka:9092') => {
+  return new Promise((resolve, reject) => {
+    const client = new kafka.KafkaClient({ kafkaHost });
+    const producer = new kafka.Producer(client);
+
+    producer.on('ready', () => {
+      const payloads = [
+        {
+          topic: topicName,
+          messages: message,
+        },
+      ];
+
+      producer.send(payloads, (err, data) => {
+        if (err) {
+          reject(err);
+        } else {
+          resolve(data);
+        }
+        producer.close();
+        client.close();
       });
     });
-  };
-  
-  module.exports = sendMessage;
-  
+
+    producer.on('error', (err) => {
+      reject(err);
+    });
+  });
+};
+
+module.exports = sendMessage;
