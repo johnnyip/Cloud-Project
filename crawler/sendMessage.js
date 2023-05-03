2ed1bf902978630f7be83418775617b40c5b9d3d
unclechuen <johnny@johnnyip.com>
2023-05-03 20:13:32 +0800

    Update sendMessage.js

--- crawler/sendMessage.js
+++ crawler/sendMessage.js
@@ -1,6 +1,6 @@
 const kafka = require('kafka-node');
 
-const sendMessage = (topicName, message, kafkaHost = 'localhost:9092') => {
+const sendMessage = (topicName, message, kafkaHost = process.env.KAFKA_URL !== undefined ? process.env.KAFKA_URL :'localhost:9092') => {
     return new Promise((resolve, reject) => {
       const client = new kafka.KafkaClient({ kafkaHost });
       const producer = new kafka.Producer(client);
