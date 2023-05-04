c7b34a55dd9d22f8b32a4a8e4cdb48c2f9641969
unclechuen <johnny@johnnyip.com>
2023-05-05 04:09:53 +0800

    Update entrypoint.sh

--- kafka-connect-mongodb/entrypoint.sh
+++ kafka-connect-mongodb/entrypoint.sh
@@ -5,11 +5,12 @@ sed -i "s|{{MONGODB_URI}}|${MONGODB_URI}|g" /etc/kafka-connect/mongo-sink.json
 # Start Kafka Connect in the background
 bash /etc/confluent/docker/run &
 
-# Wait for Kafka Connect to start
-while ! curl -s http://kafka-connect-mongo:8083/ | grep -q 'Kafka Connect'; do
+# Wait for Kafka Connect to start, or print the curled message
+while ! curl -s http://kafka-connect-mongo:8083/ | grep -q 'version'; do
   echo "Waiting for Kafka Connect to start..."
   sleep 5
 done
+
 # Deploy the MongoDB Sink Connector
 curl -X POST -H "Content-Type: application/json" -d @/etc/kafka-connect/mongo-sink.json http://kafka-connect-mongo:8083/connectors
 
