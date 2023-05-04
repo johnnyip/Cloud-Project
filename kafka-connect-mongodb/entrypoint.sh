9c650624e3c8aade67d5589d1359b9f99f4d3125
unclechuen <johnny@johnnyip.com>
2023-05-05 01:26:20 +0800

    kafka-connect added

---
+++ kafka-connect-mongodb/entrypoint.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+sed -i "s/{{MONGODB_URI}}/${MONGODB_URI}/g" /etc/kafka-connect/mongo-sink.json
+
+# Initialize MongoDB
+echo "Initializing MongoDB..."
+mongo --quiet --uri ${MONGODB_URI} <<EOF
+use kafka;
+db.createCollection("kafka_demo");
+EOF
+echo "MongoDB initialization completed."
+
+# Start Kafka Connect in the background
+/connect-distributed.sh /etc/kafka-connect/connect-distributed.properties &
+
+# Wait for Kafka Connect to start
+sleep 20
+
+# Deploy the MongoDB Sink Connector
+curl -X POST -H "Content-Type: application/json" -d @/etc/kafka-connect/mongo-sink.json http://localhost:8083/connectors
+
+# Wait for the Kafka Connect process to complete
+wait
