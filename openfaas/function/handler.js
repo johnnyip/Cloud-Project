15f3a62fd960df012eba144c778f2396a64ead82
unclechuen <johnny@johnnyip.com>
2023-05-11 13:39:04 +0800

    OpenFaaS function update

--- openfaas/function/handler.js
+++ openfaas/function/handler.js
@@ -1,51 +1,29 @@
-const { Kafka } = require('kafkajs');
-const { MongoClient } = require('mongodb');
+const MongoClient = require('mongodb').MongoClient;
 
-const kafkaBrokers = process.env.KAFKA_BROKERS || 'localhost:9092';
-const kafkaTopic = process.env.KAFKA_TOPIC || 'test-topic';
 const mongoUrl = process.env.MONGODB_URL || 'mongodb://localhost:27017';
 const mongoDbName = process.env.MONGO_DB_NAME || 'test-db';
 const mongoCollectionName = process.env.MONGO_COLLECTION_NAME || 'test-collection';
 
-const kafka = new Kafka({
-    clientId: 'kafka-mongo-function',
-    brokers: kafkaBrokers.split(',')
-});
-
-const mongoClient = new MongoClient(mongoUrl);
-
-async function saveMessageToMongo(message) {
-    await mongoClient.connect();
-    const db = mongoClient.db(mongoDbName);
-    const collection = db.collection(mongoCollectionName);
-    await collection.insertOne(message);
-    await mongoClient.close();
-}
-
-async function consumeFromKafka() {
-    const consumer = kafka.consumer({ groupId: 'kafka-mongo-function-group' });
-    await consumer.connect();
-    await consumer.subscribe({ topic: kafkaTopic });
-
-    await consumer.run({
-        eachMessage: async ({ message }) => {
-            const parsedMessage = JSON.parse(message.value.toString());
-            await saveMessageToMongo(parsedMessage);
-        }
-    });
-}
-
 module.exports = async (event, context) => {
+    // The Kafka message is supplied in the event parameter
+    const message = event.body;
+
+    console.log(`Kafka message: ${message}`);
+
+    // Connect to MongoDB
+    const url = mongoUrl
+    const client = new MongoClient(url);
+
     try {
-        await consumeFromKafka();
-        return context
-            .status(200)
-            .succeed('Kafka consumer is running and saving messages to MongoDB');
-    } catch (error) {
-        return context
-            .status(500)
-            .fail(`Error: ${error.message}`);
+        await client.connect();
+        const db = client.db(mongoDbName); // Please replace with your database name
+        const collection = db.collection(mongoCollectionName); // Please replace with your collection name
+
+        // Insert the Kafka message into the MongoDB collection
+        let doc = { message: message };
+        const result = await collection.insertOne(doc);
+        console.log(`Successfully inserted: ${result.insertedId}`);
+    } finally {
+        await client.close();
     }
-};
-
-
+}
