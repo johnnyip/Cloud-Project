24f14ad4cc8185b1fc483ec1db3e61263fe426d5
unclechuen <johnny@johnnyip.com>
2023-05-11 15:31:32 +0800

    kafka connect update

--- kafka-connect-mongodb/entrypoint.sh
+++ kafka-connect-mongodb/entrypoint.sh
@@ -7,14 +7,14 @@ sed -i "s|{{OPENFAAS_PASSWORD}}|${PASSWORD}|g" /etc/kafka-connect/http-sink.json
 bash /etc/confluent/docker/run &
 
 # Wait for Kafka Connect to start, or print the curled message
-while ! curl -s http://${CONNECT_REST_ADVERTISED_HOST_NAME}:8083/ | grep -q 'version'; do
+while ! curl -s http://localhost:8083/ | grep -q 'version'; do
   echo "Waiting for Kafka Connect to start..."
   sleep 5
 done
 
 # Deploy the MongoDB Sink Connector
-curl -X POST -H "Content-Type: application/json" -d @/etc/kafka-connect/mongo-sink.json http://${CONNECT_REST_ADVERTISED_HOST_NAME}:8083/connectors
-curl -X POST -H "Content-Type: application/json" -d @/etc/kafka-connect/http-sink.json http://${CONNECT_REST_ADVERTISED_HOST_NAME}:8083/connectors
+curl -X POST -H "Content-Type: application/json" -d @/etc/kafka-connect/mongo-sink.json http://localhost:8083/connectors
+curl -X POST -H "Content-Type: application/json" -d @/etc/kafka-connect/http-sink.json http://localhost:8083/connectors
 
 # Wait for the Kafka Connect process to complete
 wait
